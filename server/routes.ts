7b91d88c3945f4f2c3c259f84e4200b41869759b
kostiantynkovalchuk <176706484+kostiantynkovalchuk@users.noreply.github.com>
2025-08-09 13:56:25 +0200

    catch-all route added

--- server/routes.ts
+++ server/routes.ts
@@ -1,6 +1,7 @@
+// routes.ts
 import type { Express } from "express";
 import { createServer, type Server } from "http";
-import { storage } from "./storage";
+import path from "path";
 
 export async function registerRoutes(app: Express): Promise<Server> {
   // put application routes here
@@ -9,7 +10,14 @@ export async function registerRoutes(app: Express): Promise<Server> {
   // use storage to perform CRUD operations on the storage interface
   // e.g. storage.insertUser(user) or storage.getUserByUsername(username)
 
-  const httpServer = createServer(app);
+  // Catch-all handler: send back React's index.html file for non-API routes
+  app.get("*", (req, res) => {
+    // Only catch non-API routes
+    if (!req.path.startsWith("/api")) {
+      res.sendFile(path.resolve("dist/index.html"));
+    }
+  });
 
+  const httpServer = createServer(app);
   return httpServer;
 }
